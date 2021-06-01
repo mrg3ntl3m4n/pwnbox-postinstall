b7bdeabd0a0c6d31ab2ef52cf220a3ea0cbe6490
mrg3ntl3m4n <73318523+mrg3ntl3m4n@users.noreply.github.com>
2021-05-31 22:36:49 -0300

    Modify VPN info files

--- vpnbash.sh
+++ vpnbash.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-htbip=$(ip addr | grep tun0 | grep inet | grep 10. | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
+vpnip=$(ip addr | grep tun0 | grep inet | cut -d "/" -f 1 | cut -d " " -f 6)
 
-if [[ $htbip == *"10."* ]]
+if [[ ! -z $vpnip ]]
 then
-   echo "$htbip"
-else
-   echo ""
+   echo "$vpnip"
 fi
--- vpnpanel.sh
+++ vpnpanel.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-htbip=$(ip addr | grep tun0 | grep inet | grep 10. | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
-lab=$(cat /etc/openvpn/*.conf | grep "remote " | cut -d " " -f 2 | cut -d "." -f 1 | cut -d "-" -f 2-)
+vpnip=$(ip addr | grep tun0 | grep inet | cut -d "/" -f 1 | cut -d " " -f 6)
 
-if [[ $htbip == *"10."* ]]
+if [[ ! -z $vpnip ]]
 then
-   gwip=$(ip route | grep tun0 | grep via | cut -d " " -f 3)
-   ping=$(ping -c 1 $gwip -W 1 | sed '$!d;s|.*/\([0-9.]*\)/.*|\1|' | cut -c1-4)
-   echo "HTB VPN: $lab ($htbip) [$ping ms]"
+   echo "VPN: $vpnip"
 else
-   echo "HTB VPN: Disconnected"
+   echo "VPN: Disconnected"
 fi
